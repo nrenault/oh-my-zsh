e8dc379c8899991b1621c3852da6c6e14ab40126
noriade <noriade@web01.noriade.com>
2017-08-14 17:07:47 +0200

    Add copy of vimrc template

--- tools/custom-install.sh
+++ tools/custom-install.sh
@@ -76,6 +76,7 @@ main() {
 
   printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
   cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
+  cp $ZSH/templates/vimrc.template ~/.vimrc
   sed "/^export ZSH=/ c\\
   export ZSH=$ZSH
   " ~/.zshrc > ~/.zshrc-omztemp
